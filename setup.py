63cb053ead37e00fba7242120302ab8850b102e2
huanyang123 <huanwang.yang@gmail.com>
2021-08-14 13:48:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 
 setuptools.setup(
-	name = 'ML_tune', #this must be unique
+	name = 'ML', #this must be unique
 #	include_package_data=True,
 	version = '0.1',
 	author = 'Huanwang (Henry) Yang',

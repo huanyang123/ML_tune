8ea21cacde79ebb6080cd8671d1bc3b702dcef50
huanyang123 <huanwang.yang@gmail.com>
2021-08-14 13:48:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,13 @@ setuptools.setup(
 	author_email = 'huanwang.yang@gmail.com',
 	description = 'The ML untility modules',
 	long_description = long_description,
-	long_description_content_type = 'text/markdown' ,
+	long_description_content_type = 'text/markdown' # ,
 	#packages = setuptools.find_packages(),
+	'''
 	classifiers = [
 	'Programming Language :: Python :: 3',
 	'License :: OSI Aproved :: MIT License',
 	"Operating System :: OS Independent"],
 	python_requires = '>=3.5'
+	'''
 	)

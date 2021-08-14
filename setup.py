888c9796b05e6792eae6dd51e70680867a65387c
huanyang123 <huanwang.yang@gmail.com>
2021-08-14 13:07:42 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'ML_tune', #this must be unique
+	include_package_data=True,
+	version = '0.1',
+	author = 'Huanwang (Henry) Yang',
+	author_email = 'huanwang.yang@gmail.com',
+	description = 'The ML untility modules',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
